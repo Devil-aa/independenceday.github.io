80fff43732e4a43e4826868c2caae93754102aed
abhiraj540 <61726538+abhiraj540@users.noreply.github.com>
2020-08-14 12:36:18 +0530

    Create Javas.js

---
+++ Javas.js
@@ -0,0 +1,15 @@
+window.addEventListener("mouseover",play);
+
+window.addEventListener("click",play);
+
+function play() 
+
+{ document.getElementById("a").play();
+
+}
+
+alert ("Happy Independence")
+
+alert ("first touch screen")
+
+alert ("Let's start singing Rastrageet")
